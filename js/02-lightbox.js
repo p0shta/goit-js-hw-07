189b2861ff1a8af894a71f418785a7b856106d5d
Maksym Pshenychnyi <maksim_pshenichniy@mail.ru>
2022-04-19 21:48:48 +0300

    add 2 hometask

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryEl.innerHTML = galleryItemsMarkup;
+
+function createGalleryItemsMarkup(items) {
+    const markup = items
+        .map(({ preview, original, description }) => {
+            return `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>`;
+        })
+        .join('');
+
+    return markup;
+}
+
+let gallery = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt' });
+gallery.on('show.simplelightbox', function () {});
